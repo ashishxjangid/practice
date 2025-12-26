bea2c3081f4bab835c0dadf372077efc332c56f2
Ashish Jangid <129286263+ashishxjangid@users.noreply.github.com>
2025-12-26 15:41:01 +0530

    Update category.js

--- category.js
+++ category.js
@@ -1,15 +1,20 @@
-// const colorFilter= document.querySelector(".sg__swatch_options");
+// const colorFilter= document.querySelector("#sg__swatch_option-0");
 
 // const clonedColorFilter= colorFilter.cloneNode(true);
-// clonedColorFilter.style.setProperty('display', 'block', 'important');
+// clonedColorFilter.id= "colorFilter";
 
 // document.body.append(clonedColorFilter);
 
+// const sizeFilter= document.querySelector("#sg__swatch_option-1");
 
+// const clonedSizeFilter= sizeFilter.cloneNode(true);
 
-// const colorFilter= document.querySelector(".sg__swatch_options");
+// document.body.append(clonedSizeFilter);
 
-// const clonedColorFilter= colorFilter.cloneNode(true);
-// clonedColorFilter.setAttribute( 'style', 'display: block !important' );
-
-// document.body.append(clonedColorFilter);
+// const buttons= document.querySelectorAll("#colorFilter .sg__swatch_option-item");
+// buttons.forEach((btn) => {
+//     btn.addEventListener("click", ()=> {
+//         buttons.forEach(b => b.classList.remove("sg__swatch-active"));
+//         btn.classList.add("sg__swatch-active");
+//     })
+// })
